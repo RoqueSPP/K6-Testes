f572664868507b535fa8653ca7e15195ebd1a423
RoqueSPP <79621434+RoqueSPP@users.noreply.github.com>
2026-09-24 11:06:08 -0300

    Add vus option to k6 test configuration

--- k6_teste.js
+++ k6_teste.js
@@ -3,6 +3,7 @@ import { sleep } from 'k6';
 
 export const options = {
   iterations: 10,
+    vus: 1
 };
 
 // The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
