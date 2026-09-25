d6fdd39eff40423d4d718bf81c359495136e2286
RoqueSPP <79621434+RoqueSPP@users.noreply.github.com>
2026-09-25 10:29:32 -0300

    Update k6_teste.js

--- scripts/k6_teste.js
+++ scripts/k6_teste.js
@@ -4,7 +4,7 @@ import { sleep } from 'k6';
 
    export function handleSummary(data) {
      return {
-       "relatorio.html": htmlReport(data),
+       "index.html": htmlReport(data),
      };
    }
 
